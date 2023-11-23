1bdc4f43d03162f899e94accb8044d47eeca57f6
Wesley <Wesleyrose06@gmail.com>
2023-11-23 10:27:08 -0500

    Update Package.json

--- dist/actions.js
+++ dist/actions.js
@@ -2,14 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getUsersInRoom = exports.removeUser = exports.addUser = void 0;
 var users = [];
-/* The `addUser` function is a function that takes an object with `id`, `room`, and `name` properties
-as its argument. It then checks if the `name` and `room` properties are truthy (not empty or
-undefined), and if not, it returns an error object with a message. It also checks if there is
-already a user with the same `name` and `room` in the `users` array, and if so, it returns an error
-object with a message. If there are no errors, it creates a new `user` object with the `id`, `room`,
-and `name` properties, adds it to the `users` array, and returns an object with the `user` property
-set to the newly created user object. The `name` and `room` properties are also trimmed and
-converted to lowercase before being used. */
+/**
+ * This function adds a new user to a chat room if the username is not taken and both the username and
+ * room are provided.
+ * @param  - The function `addUser` takes an object as its parameter with three properties:
+ * @returns The function `addUser` returns an object with a `user` property if the user was
+ * successfully added to the `users` array. If there was an error, it returns an object with an `error`
+ * property.
+ */
 var addUser = function (_a) {
     var id = _a.id, room = _a.room, name = _a.name;
     name = name.trim().toLowerCase();
@@ -23,17 +23,28 @@ var addUser = function (_a) {
     users.push(user);
     return { user: user };
 };
-/* This code is defining and exporting two functions, `addUser` and `removeUser`, as part of a module. */
 exports.addUser = addUser;
+/**
+ * This function removes a user from an array of users based on their ID.
+ * @param id - The `id` parameter is a unique identifier for a user that needs to be removed from an
+ * array of users. The function uses this `id` to find the index of the user in the `users` array using
+ * the `findIndex` method. If the user is found, the function removes
+ * @returns the user object that was removed from the `users` array. If no user with the specified `id`
+ * is found in the array, the function returns `undefined`.
+ */
 var removeUser = function (id) {
     var index = users.findIndex(function (user) { return user.id === id; });
     if (index !== -1) {
         return users.splice(index, 1)[0];
     }
 };
-/* These lines of code are exporting the `removeUser` and `getUsersInRoom` functions as part of the
-module. This allows other parts of the code to import and use these functions. */
 exports.removeUser = removeUser;
+/**
+ * This function filters an array of users to return only those who are in a specific room.
+ * @param room - The `room` parameter is a string that represents the name or ID of a chat room. The
+ * function `getUsersInRoom` filters an array of `users` and returns an array of users who are
+ * currently in the specified `room`. The `user` objects in the `users` array
+ */
 var getUsersInRoom = function (room) { return users.filter(function (user) { return user.room === room; }); };
 exports.getUsersInRoom = getUsersInRoom;
 //# sourceMappingURL=actions.js.map
--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,12 @@ var cors = require("cors");
 var http_1 = require("http");
 var socket_io_1 = require("socket.io");
 var actions_1 = require("./actions");
+/* This code is setting up a basic server using the Express framework in Node.js. It creates an
+instance of the Express application, sets the port to either the value of the `PORT` environment
+variable or 8000 if it is not defined, enables Cross-Origin Resource Sharing (CORS) using the
+`cors()` middleware, creates an HTTP server using the `createServer()` method from the Node.js
+`http` module, and creates a new instance of the Socket.IO server using the `Server()` constructor.
+The `cors` option in the Socket.IO server configuration allows all origins to access the server. */
 var app = express();
 var port = process.env.PORT || 8000;
 app.use(cors());
@@ -14,8 +20,6 @@ var io = new socket_io_1.Server(server, {
         origin: '*'
     }
 });
-/* This code block is creating a socket connection and handling various events related to the chat
-application. */
 io.on("connection", function (socket) {
     console.log("connection created");
     // Join a conversation
@@ -38,6 +42,10 @@ io.on("connection", function (socket) {
     socket.on("stop typing message", function (data) {
         io.in(user.room).emit("stop typing message", data);
     });
+    /* This code is listening for a "disconnect" event on the socket, which is triggered when a client
+    disconnects from the server. When this event is triggered, the code logs a message to the console
+    indicating which user has left the chat, removes the user from the list of users in the room, emits
+    a "user leave chat" event to all clients in the room, and removes the socket from the room. */
     socket.on("disconnect", function () {
         console.log(socket.id + ": (" + user.name + ") left chat!");
         actions_1.removeUser(socket.id);
@@ -45,16 +53,14 @@ io.on("connection", function (socket) {
         socket.leave(user.room);
     });
 });
-/* This code block is creating a route for the root URL of the server. When a GET request is made to
-the root URL, the server responds with a message "Your on the backend Mr.Maur Make sure you run
-'yarn start' in the FrontEnd Directory". */
+/* This code is setting up a basic HTTP server using the Express framework in Node.js. The `app.get()`
+method is defining a route for the root URL (`/`) and sending a response to the client. The
+`server.listen()` method is starting the server and listening for incoming requests on the specified
+port. When the server starts, it logs a message to the console indicating which port it is running
+on. */
 app.get('/', function (req, res) {
     res.send("Your on the backend Mr.Maur Make sure you run 'yarn start' in the FrontEnd Directory");
 });
-/* `server.listen(port, function () { return console.log("App running on port " + port + "."); });` is
-starting the server and listening for incoming requests on the specified port. When the server
-starts running, it logs a message to the console indicating that the app is running on the specified
-port. */
 server.listen(port, function () {
     return console.log("App running on port " + port + ".");
 });
